f3a331a6fac9af8ce3c15b65a3ea00197f205a38
MathijsDev <migielklaver@gmail.com>
2024-06-23 11:50:10 +0200

    * Began work on new UI

---
+++ README.md
@@ -0,0 +1,4 @@
+# THEO 32 Simulator
+Theo 32 is a custom designed CPU architecture. This CPU yet to be build in real life, this simulator is just for testing purposes.
+
+[Github to THEO32](https://github.com/MathijssYDev/THEO-32)
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import UI.MemoryInterface;
 import theo32machine.THEO32;
 
 public class Main {
     public static void main(String args[]) throws Exception{
         if (args.length != 3) throw new Exception("(Main Thread) Missing Arguments! Expected arguments: File location ROM, File location Bootloader, COMPORT to arduino (AY-3-8910)");
 
-        THEO32 theo32 = new THEO32("ROM/"+args[0],"ROM/" +args[1],Integer.parseInt(args[2]),1000);
+        THEO32 theo32 = new THEO32("ROM/"+args[0],"ROM/" +args[1],Integer.parseInt(args[2]),10000, false);
+        MemoryInterface meminterface = new MemoryInterface(theo32);
+
         theo32.start();
+
     }
 }
